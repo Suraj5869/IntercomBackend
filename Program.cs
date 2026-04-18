a9429547a766db2d14386a699d054d6e78797e09
Suraj Sakhare <120657694+Suraj5869@users.noreply.github.com>
2026-04-18 14:56:12 +0530

    Set application to listen on port 8080

--- Program.cs
+++ Program.cs
@@ -29,6 +29,7 @@ namespace RiderIntercom
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.WebHost.UseUrls("http://0.0.0.0:8080");
             builder.Services.AddScoped<IDbConnectionFactory, DbConnectionFactory>();
             builder.Services.AddScoped<AuthRepository>();
             builder.Services.AddScoped<RoomRepository>();
