c43c829e8f0473e14178798af87869e04799200b
Suraj Sakhare <120657694+Suraj5869@users.noreply.github.com>
2026-04-18 21:52:50 +0530

    Disable Swagger in development environment
    Comment out development environment checks for Swagger.

--- Program.cs
+++ Program.cs
@@ -35,11 +35,11 @@ namespace RiderIntercom
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            // if (app.Environment.IsDevelopment())
+            // {
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            // }
             app.UseCors("AllowAngular");
             //app.UseHttpsRedirection();
 
